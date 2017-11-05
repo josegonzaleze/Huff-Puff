331ebb10208825ddfcaf1163ca0d4dafb6b13025
nburner <nburner@harding.edu>
2017-11-05 01:21:10 -0600

    Refactoring/Adding Puff

---
+++ Puff/Puff/Puff.cpp
@@ -0,0 +1,19 @@
+// Authors: Jose Gonzales and Nathan Burner
+
+#include<fstream>
+#include<iostream>
+#include<iomanip>
+#include<string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+void main()
+{
+	string inFileName = "";
+	cout << "Enter the name of the file to decompress: ";
+	cin >> inFileName;
+	cout << inFileName;
+}
